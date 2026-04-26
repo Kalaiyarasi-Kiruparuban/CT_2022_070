bbd275005eda378810e7c7d519f887a78ee72506
Kalaiyarasi Kiruparuban <kirupar-ct22070@stu.kln.ac.lk>
2026-04-26 22:33:57 +0530

    Update Q1.java

--- Q1.java
+++ Q1.java
@@ -4,7 +4,7 @@ public class Q1 {
     public static void main() {
         JFrame window = new JFrame();
         window.setSize(800, 600);
-        window.setTitle("Kalaiyarasi");
+        window.setTitle("Welcome to Java");
         window.setVisible(true);
     }
 }
